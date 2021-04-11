8babfd51e2697187ca13a1ed8a91280903a973ec
Nayamin <nayamin.git@gmail.com>
2021-04-11 17:59:33 -0500

    remove config files

---
+++ routes/route.js
@@ -0,0 +1,16 @@
+import questionCtrl from '../controller/question-ctrl.js';
+//import createVersion from '../create-version.js';
+
+const router = {};
+
+router.route = (app) => {
+    app.get('/api/v1/questions', questionCtrl.getAllQuestions);
+    app.get('/api/v1/user/:userId/questions', questionCtrl.getUserQuestions);
+    app.put('/api/v1/user/:userId/questions', questionCtrl.updateQuestions);
+    app.get('/api/v1/user/:userId/info', questionCtrl.getUserInfo);
+    app.get('/api/v1/user', questionCtrl.getAllUser);
+    app.post('/api/v1/user', questionCtrl.addUser);
+    //app.get('/api/v1/versions', createVersion.getVersion);
+};
+
+export default router;
